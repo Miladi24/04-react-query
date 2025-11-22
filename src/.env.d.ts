92abe61e733006c60549b74189aba24d9cc82eec
Mila <miladiachenko2404@gmail.com>
2025-11-22 12:38:14 +0200

    Initial commit

---
+++ src/.env.d.ts
@@ -0,0 +1,6 @@
+interface ImportMetaEnv {
+  readonly VITE_TMBD_TOKEN: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App/App";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "modern-normalize/modern-normalize.css";
+
+const container = document.getElementById("root");
+if (!container) throw new Error("Root element not found.");
+
+const root = createRoot(container);
+
+// Ініціалізуємо клієнт React Query
+const queryClient = new QueryClient();
+
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster position="top-right" reverseOrder={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
